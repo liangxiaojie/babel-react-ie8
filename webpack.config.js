74faa9e2ab47fad909d8e5893ef0bd2904c7675a
liangxiaojie <15201515601@163.com>
2017-09-20 11:30:41 +0800

    transform es2015+ to support  IE 5+

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require('path'),
+  webpack = require('webpack');
+
+var DEBUG = process.env.NODE_ENV === 'development';
+
+var plugins = [];
+
+if (!DEBUG) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      properties: false,
+      warnings: false
+    },
+    output: {
+      quote_keys: true
+    },
+    mangle: {
+      screw_ie8: false
+    },
+    sourceMap: false
+  }));
+}
+
+module.exports = {
+  entry: [
+    'babel-polyfill', './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ],
+    postLoaders: [
+      {
+        test: /\.js$/,
+        loaders: ['es3ify-loader']
+      }
+    ]
+  },
+  plugins: plugins,
+  debug: DEBUG,
+  devtool: DEBUG
+    ? 'source-map'
+    : false
+};
