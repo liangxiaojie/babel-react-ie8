fc642c766545e55fe0690c488757f6be7642b768
liangxiaojie <liangxiaojie@genedock.com>
2017-09-22 10:44:42 +0800

    update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
+  parser: 'babel-eslint',
+  extends: 'airbnb',
   env: {
     browser: true,
-    "node": true
+    node: true
   },
-  extends: 'eslint-config-airbnb',
-  parser: 'babel-eslint',
   rules: {
     'linebreak-style': 0,
     'react/jsx-filename-extension': 0,
